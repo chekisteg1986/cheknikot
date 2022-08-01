3f6d9202e10fd964a85d4d20ea1fd7fe109c1afd
chekisteg1986 <chekisteg@gmail.com>
2022-08-01 10:30:05 +0300

    ds

--- cheknikot/char_additions/CharBuffBasic.hx
+++ cheknikot/char_additions/CharBuffBasic.hx
@@ -1,37 +1,29 @@
 package cheknikot.char_additions;
 
-import cheknikot.game_objects.BaseCharacter;
 import flixel.effects.particles.FlxEmitter;
+
 /**
  * ...
  * @author ...
  */
 class CharBuffBasic
 {
-
-
-	
 	public var type:Int = 0;
 	public var attributes:CharAttributesBasic;
 	public var emitter:FlxEmitter;
-	public function new(_char:CharAttributesBasic) 
+
+	public function new(_char:CharAttributesBasic)
 	{
 		attributes = _char;
 		attributes.buffs.push(this);
 	}
-	
+
 	public var time:Float = 10;
-	
-	public function actions(_elapsed:Float):Void
-	{
-		
-		
-	}
-	
+
+	public function actions(_elapsed:Float):Void {}
+
 	public function remove():Void
 	{
 		attributes.buffs.remove(this);
-		
 	}
-	
 }
